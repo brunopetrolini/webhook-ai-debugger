7847e67c9d9be1b85b79fc0014dc2a1313334b95
Bruno Petrolini <brunopetrolini@hotmail.com>
2025-10-27 11:37:31 -0300

    style(server): format code for consistency and improved readability

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,15 +1,15 @@
-import { fastifyCors } from '@fastify/cors';
-import { fastifySwagger } from '@fastify/swagger';
-import ScalarApiReference from '@scalar/fastify-api-reference';
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import ScalarApiReference from "@scalar/fastify-api-reference";
 import { fastify } from "fastify";
 import {
   jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
-} from 'fastify-type-provider-zod';
-import { env } from './env';
-import { listWebhooks } from './routes/list-webhooks';
+} from "fastify-type-provider-zod";
+import { env } from "./env";
+import { listWebhooks } from "./routes/list-webhooks";
 
 const app = fastify();
 
@@ -18,30 +18,33 @@ app.setSerializerCompiler(serializerCompiler);
 app.withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 });
 
 app.register(fastifySwagger, {
   openapi: {
     info: {
-      title: 'Webhook Inspector API',
-      description: 'API for inspecting webhooks requests',
-      version: '1.0.0',
+      title: "Webhook Inspector API",
+      description: "API for inspecting webhooks requests",
+      version: "1.0.0",
     },
   },
   transform: jsonSchemaTransform,
 });
 
-app.register(ScalarApiReference, { routePrefix: '/docs', configuration: {
-  hideDarkModeToggle: true,
-  theme: 'deepSpace'
-} });
+app.register(ScalarApiReference, {
+  routePrefix: "/docs",
+  configuration: {
+    hideDarkModeToggle: true,
+    theme: "deepSpace",
+  },
+});
 
 /**
  * Routes registration
  */
-const defaultPrefix = '/api';
+const defaultPrefix = "/api";
 app.register(listWebhooks, { prefix: defaultPrefix });
 
 app.listen({ port: env.PORT, host: env.HOST }, (err, address) => {
